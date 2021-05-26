3763c7e4b00c6437781cf478251ac57928615d07
amneetkhaira <74328077+amneetkhaira@users.noreply.github.com>
2021-05-25 21:35:26 -0700

    maxFeature

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from max import calculate_max
+
 def create_list():
     a_list = []
     for i in range(9):
         x = a_list.append(int(input("Please enter an integer: ")))
               
 create_list()
+
+calculate_max()
---
+++ max.py
@@ -0,0 +1,7 @@
+def calculate_max():
+    a_list=[]
+    for i in range(9):
+        x = a_list.append(int(input("Please enter an interger: ")))
+        maxNum = float(max(a_list))
+    print()
+    print("The max number is: ", maxNum)
